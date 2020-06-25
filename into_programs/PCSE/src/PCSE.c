eb88c620eba9c2d25751c4eafae6bfd7aaa649d4
Luchao90 <57074169+Luchao90@users.noreply.github.com>
2020-06-25 20:49:00 -0300

    TP FINAL-BMP280
    Se agregan todos los archivos relacionados al código y modificaciones del firmware_v3 para el DRIVER BMP280

---
+++ into_programs/PCSE/src/PCSE.c
@@ -0,0 +1,105 @@
+/*=============================================================================
+ * Author: Luchao90 <lucianovittori99@gmail.com>
+ * Date: 2020/06/16
+ * Version: Sapi
+ *===========================================================================*/
+
+/*=====[Inclusions of function dependencies]=================================*/
+
+#include "PCSE.h"
+#include "sapi.h"
+
+/*=====[Definition macros of private constants]==============================*/
+
+/*=====[Definitions of extern global variables]==============================*/
+
+/*=====[Definitions of public global variables]==============================*/
+
+/*=====[Definitions of private global variables]=============================*/
+
+/*=====[Main function, program entry point after power on or reset]==========*/
+
+int main(void)
+{
+    boardInit();
+    i2cInit(I2C0, 100000);
+    // SPI configuration
+    spiConfig( SPI0 );
+    BMP280_CS_HIGH();
+
+    int8_t rslt;
+    struct bmp280_dev bmp;
+    struct bmp280_config conf;
+    struct bmp280_uncomp_data ucomp_data;
+    uint32_t pres32, pres64;
+    double pres;
+
+    /* Map the delay function pointer with the function responsible for implementing the delay */
+    bmp.delay_ms = delay_ms;
+
+//    /* Assign device I2C address based on the status of SDO pin (GND for PRIMARY(0x76) & VDD for SECONDARY(0x77)) */
+//    bmp.dev_id = BMP280_I2C_ADDR_PRIM;
+//
+//    /* Select the interface mode as I2C */
+//    bmp.intf = BMP280_I2C_INTF;
+//
+//    /* Map the I2C read & write function pointer with the functions responsible for I2C bus transfer */
+//    bmp.read = i2c_reg_read;
+//    bmp.write = i2c_reg_write;
+
+    /* To enable SPI interface: comment the above 4 lines and uncomment the below 4 lines */
+
+     bmp.dev_id = 0;
+     bmp.read = spi_reg_read;
+     bmp.write = spi_reg_write;
+     bmp.intf = BMP280_SPI_INTF;
+
+    rslt = bmp280_init(&bmp);
+    print_rslt(" bmp280_init status", rslt);
+
+    /* Always read the current settings before writing, especially when
+     * all the configuration is not modified
+     */
+    rslt = bmp280_get_config(&conf, &bmp);
+    print_rslt(" bmp280_get_config status", rslt);
+
+    /* configuring the temperature oversampling, filter coefficient and output data rate */
+    /* Overwrite the desired settings */
+    conf.filter = BMP280_FILTER_COEFF_2;
+
+    /* Pressure oversampling set at 4x */
+    conf.os_pres = BMP280_OS_4X;
+
+    /* Setting the output data rate as 1HZ(1000ms) */
+    conf.odr = BMP280_ODR_1000_MS;
+    rslt = bmp280_set_config(&conf, &bmp);
+    print_rslt(" bmp280_set_config status", rslt);
+
+    /* Always set the power mode after setting the configuration */
+    rslt = bmp280_set_power_mode(BMP280_NORMAL_MODE, &bmp);
+    print_rslt(" bmp280_set_power_mode status", rslt);
+    while (1)
+    {
+        /* Reading the raw data from sensor */
+        rslt = bmp280_get_uncomp_data(&ucomp_data, &bmp);
+
+        /* Getting the compensated pressure using 32 bit precision */
+        rslt = bmp280_get_comp_pres_32bit(&pres32, ucomp_data.uncomp_press, &bmp);
+
+        /* Getting the compensated pressure using 64 bit precision */
+        rslt = bmp280_get_comp_pres_64bit(&pres64, ucomp_data.uncomp_press, &bmp);
+
+        /* Getting the compensated pressure as floating point value */
+        rslt = bmp280_get_comp_pres_double(&pres, ucomp_data.uncomp_press, &bmp);
+        printf("UP: %ld, P32: %ld, P64: %ld, P64N: %ld, P: %f\r\n",
+               ucomp_data.uncomp_press,
+               pres32,
+               pres64,
+               pres64 / 256,
+               pres);
+        bmp.delay_ms(1000); /* Sleep time between measurements = BMP280_ODR_1000_MS */
+    }
+
+    return 0;
+}
+
